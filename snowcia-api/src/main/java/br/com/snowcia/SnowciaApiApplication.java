d5d181e5afa24e864458b4d6bddf156bcc6b66a0
EvandroKaibara <yujievandro@gmail.com>
2026-07-16 11:17:39 -0300

    feat(core): initialize Spring Boot project structure

---
+++ snowcia-api/src/main/java/br/com/snowcia/SnowciaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.snowcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowciaApiApplication.class, args);
+	}
+
+}
---
+++ snowcia-api/src/test/java/br/com/snowcia/SnowciaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.snowcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowciaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
